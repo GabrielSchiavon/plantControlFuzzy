5cb2363f6ecdb9ee2008cdbf238a6c7e1dfbf753
schiavon <schiavon@schiavon>
2016-10-06 18:23:42 -0300

    Ajustes realizados.

--- src/greenhousefuzzy/GreenhouseFuzzy.java
+++ src/greenhousefuzzy/GreenhouseFuzzy.java
@@ -1,9 +1,9 @@
 package greenhousefuzzy;
 
 import net.sourceforge.jFuzzyLogic.FIS;
-import net.sourceforge.jFuzzyLogic.FunctionBlock;
 import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
 import net.sourceforge.jFuzzyLogic.rule.Variable;
+import net.sourceforge.jFuzzyLogic.*;
 
 public class GreenhouseFuzzy {
 
@@ -14,35 +14,35 @@ public class GreenhouseFuzzy {
             System.err.println("Can't load file: '" + fileName + "'");
             return;
         }
-        
-        //FunctionBlock functionBlock = fis.getFunctionBlock(null);
-        JFuzzyChart.get().chart(fis);
+                
+        FunctionBlock functionBlock = fis.getFunctionBlock(null);
+        JFuzzyChart.get().chart(functionBlock);
         
         fis.setVariable("time", 8);
         fis.setVariable("humidity", 65);
         fis.setVariable("sensation", 45);
         fis.setVariable("absorptance", 60);
-        fis.setVariable("soilHumidity", 45);
+        fis.setVariable("soilHumidity", 38);
         
         fis.evaluate();
         
-        Variable health = fis.getVariable("health");
+        Variable health = functionBlock.getVariable("health");
         JFuzzyChart.get().chart(health, health.getDefuzzifier(), true);
         
-        Variable cooler = fis.getVariable("cooler");
+        Variable cooler = functionBlock.getVariable("cooler");
         JFuzzyChart.get().chart(cooler, cooler.getDefuzzifier(), true);
         
-        Variable solenoid = fis.getVariable("solenoid");
+        Variable solenoid = functionBlock.getVariable("solenoid");
         JFuzzyChart.get().chart(solenoid, solenoid.getDefuzzifier(), true);
         
-        Variable light = fis.getVariable("light");
+        Variable light = functionBlock.getVariable("light");
         JFuzzyChart.get().chart(light, light.getDefuzzifier(), true);
         
         System.out.println(fis);
-        System.out.println("Health:" + fis.getVariable("health").getValue());
-        System.out.println("cooler:" + fis.getVariable("cooler").getValue());
-        System.out.println("solenoid:" + fis.getVariable("solenoid").getValue());
-        System.out.println("light:" + fis.getVariable("light").getValue());
+        System.out.println("Health:" + functionBlock.getVariable("health").getValue());
+        System.out.println("cooler:" + functionBlock.getVariable("cooler").getValue());
+        System.out.println("solenoid:" + functionBlock.getVariable("solenoid").getValue());
+        System.out.println("light:" + functionBlock.getVariable("light").getValue());
     }
     
 }
